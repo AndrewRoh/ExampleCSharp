e6243b480912fed0436033ca6a500b2bd3dfe44a
AndrewRoh <nohgyuwon@gmail.com>
2021-08-07 15:00:14 +0900

    couchbase 7.0에 맞추어 코드 수정 및 추가

--- ExamCouchbase/ExamQuery.md
+++ ExamCouchbase/ExamQuery.md
@@ -2,7 +2,63 @@
 * "defer_build":true 백그라운드에서 인덱스를 생성함
 * primary key 존재하는지 체크
 ```mysql
+CREATE PRIMARY INDEX `beer_primary` ON `beer-sample` USING GSI;
+select * from `beer-sample` data where type='beer' order by meta().id limit 10 offset 0
+
+CREATE PRIMARY INDEX `beer_primary` ON `beer-sample` WITH { "defer_build":true }
+select * from `beer-sample`.`_default`.`_default` data where type='beer' order by meta().id limit 10 offset 0
+
 CREATE PRIMARY INDEX `def_primary` ON `travel-sample` WITH {  "defer_build":true }
 CREATE PRIMARY INDEX idx_airport_primary ON airport USING GSI;
 SELECT * FROM system:indexes WHERE name="idx_hotel_primary";
+
+
+SELEC `kr-center-logs_primary` ON `kr-center-logs` WITH {  "defer_build":true }
+
+CREATE PRIMARY INDEX `kr-center-logs_primary` ON `kr-center-logs` USING GSI;
+select * from `kr-center-logs` data where kind='t_error' order by meta().id limit 10 offset 100
+
+CREATE PRIMARY INDEX `kr-regio1-logs_primary` ON `kr-region1-logs` USING GSI;
+select * from `kr-region1-logs` where LogKind=12 and date > '2021-07-01 00:00:00' and item_id=303002000 order by meta().date limit 500 offset 500
+
+18초
+
+CREATE INDEX `kr-regio1-logs_idx_consume` ON `kr-region1-logs`(`date`,`item_id`) WHERE(`LogKind`=12) USING GSI;
+
+select * from `kr-region1-logs` USE INDEX (`kr-regio1-logs_idx_consume`) where LogKind=12 and date > '2021-07-01 00:00:00' and item_id=303002000 order by meta().date limit 500 offset 500
+
+select data.* from `kr-region1-logs` data where LogKind=12 and date > '2021-07-01 00:00:00' and item_id=303002000 order by meta().date limit 500 offset 500
+
+/opt/couchbase/bin/cbbackup -u Administrator -p tmxpdj http://127.0.0.1:8091 -b kr-center-logs ~/backup-43/
+scp -i ~/stairgames.pem -r ec2-user@3.35.166.192:~/backup-43/* /home/stair/backup-43/
+/opt/couchbase/bin/cbrestore ~/backup-43 http://127.0.0.1:8091 -u Administrator -p tmxpdj -b kr-center-logs
+--------------------------------------------
+ 
+/opt/couchbase/bin/cbbackup -u Administrator -p tmxpdj http://127.0.0.1:8091 -b kr-region1-logs ~/backup-43/
+scp -i ~/stairgames.pem -r ec2-user@3.35.166.192:~/backup-43/* /home/stair/backup-43/
+/opt/couchbase/bin/cbrestore ~/backup-43 http://127.0.0.1:8091 -u Administrator -p tmxpdj -b kr-region1-logs
+
+{
+  "LogKind": 12,
+  "log_uid": null,
+  "date": "2021-07-21T14:50:26.458004+09:00",
+  "transaction_log_uid": 2021072114502610218,
+  "region_index": 1,
+  "account_uid": 449,
+  "character_uid": 100000000000000632,
+  "character_name": "\uB358\uC804\uAC00\uC988\uC544",
+  "type": "Get",
+  "event_type": "BossReward(dungeon:6000800,level:0)",
+  "count": 6,
+  "world_type": "Dungeon",
+  "channel_index": 6000800,
+  "channel_uid": 37,
+  "item_uid": 100000000035900049,
+  "item_id": 303002000,
+  "before_count": 242,
+  "after_count": 248,
+  "extra_log": "",
+  "kind": "t_use_get_consume_item"
+}
+
 ```
--- ExamCouchbase/Program.cs
+++ ExamCouchbase/Program.cs
@@ -1,36 +1,105 @@
 ﻿using Couchbase;
-using Couchbase.Search.Queries.Compound;
-using Couchbase.Search.Queries.Range;
-using Couchbase.Search.Queries.Simple;
+using Couchbase.Management.Buckets;
+using Couchbase.Management.Collections;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ExamCouchbase
 {
     class Program
     {
+        //async Task<IBucket> GetIBucket(string bucketName)
+        //{
+
+        //}
+
         static async Task Main()
         {
             try
             {
-                var cluster = await Cluster.ConnectAsync("couchbase://192.168.0.11", "user1", "password");
+                string connectionString = "couchbase://192.168.0.21";
+                string username = "Administrator";
+                string password = "tmxpdj";
+                string bucketName = "kr-region1-logs";
+                string scopeSpecName = "logs_region1";
+                string collectionSpecName = "log_kind_01";
 
-                // get a bucket reference
-                var bucket = await cluster.BucketAsync("travel-sample");
+                // 클러스터 연결 (Server = Cluster)                
+                var iCluster = await Cluster.ConnectAsync(connectionString, username, password);
 
-                var scope = bucket.Scope("inventory");
-                var collection = scope.Collection("airline");
+                // 버킷 생성 확인 (Database = Bucket) iCluster.BucketAsync()은 Exception이 발생하므로 확인 불가
+                var bucketManager = iCluster.Buckets;
+                var bucketSettings = bucketManager.GetAllBucketsAsync();
+                if (bucketSettings.Result.ContainsKey(bucketName) is false)
+                {
+                    BucketSettings settings = new()
+                    {
+                        Name = bucketName,
+                        BucketType = BucketType.Couchbase,
+                        RamQuotaMB = 512,
+                        FlushEnabled = false,
+                        NumReplicas = 0,
+                        ReplicaIndexes = false,
+                        ConflictResolutionType = ConflictResolutionType.SequenceNumber,
+                        EvictionPolicy = EvictionPolicyType.ValueOnly,
+                        //EjectionMethod = EvictionPolicyType.ValueOnly,
+                        MaxTtl = 0,
+                        CompressionMode = CompressionMode.Off,
+                        DurabilityMinimumLevel = Couchbase.KeyValue.DurabilityLevel.None
+                    };
 
-                // get a collection reference
-                // var collection = bucket.DefaultCollection();
+                    await bucketManager.CreateBucketAsync(settings, default);
 
-                // get document from collection
-                var strKey = $"airline_10";
-                var getResult = await collection.GetAsync(strKey);
-                Console.WriteLine($"getResult cas: {getResult.Cas}\n{getResult.ContentAs<dynamic>()}");
+                    // 생성후 컬렉션을 사용할 경우 Eception발생 Ready 상태 확인 필요 iBucket.Collections 
+                    //await Task.Delay(TimeSpan.FromSeconds(7));
+                    await iCluster.WaitUntilReadyAsync(TimeSpan.FromSeconds(10));
+                }
+
+                // 버킷 연결 (Database = Bucket)
+                var iBucket = await iCluster.BucketAsync(bucketName);
+                if (iBucket is null)
+                {
+                    Console.WriteLine($"ERROR iBucket: {bucketName}");
+                    return;
+                }
+
+                // 스코프 생성 확인 (Schema = Scope) iCollectionMgr.GetScopeAsync() Exception, iBucket.Scope()은 실제 Scope가 없어도 넘어가므로 확인 불가
+                var iCollectionMgr = iBucket.Collections;
+                var scopeSpecs = await iCollectionMgr.GetAllScopesAsync();
+
+                List<string> scopeSpecNames = new();
+                foreach (var scope in scopeSpecs)
+                {
+                    scopeSpecNames.Add(scope.Name);
+                }
+                if (scopeSpecNames.Contains(scopeSpecName) is false)
+                {
+                    await iCollectionMgr.CreateScopeAsync(scopeSpecName);
+                }
+
+                var scopeSpec = await iCollectionMgr.GetScopeAsync(scopeSpecName);
+
+                List<string> collectionNames = new();
+                foreach (var collectionSpec in scopeSpec.Collections)
+                {
+                    collectionNames.Add(collectionSpec.Name);
+                }
+
+                if (collectionNames.Contains(collectionSpecName) is false)
+                {
+                    var collectionSpec = new CollectionSpec(scopeSpecName, collectionSpecName);
+                    await iCollectionMgr.CreateCollectionAsync(collectionSpec);
+                }
+
+
+                //// get document from collection
+                //var strKey = $"airline_10";
+                //var getResult = await collection.GetAsync(strKey);
+                //Console.WriteLine($"getResult cas: {getResult.Cas}\n{getResult.ContentAs<dynamic>()}");
 
                 // get documents from cluster QueryAsync
-                var queryResult = await cluster.QueryAsync<dynamic>("SELECT t.* FROM `travel-sample` t WHERE t.type=$1 LIMIT 5",
+                var queryResult = await iCluster.QueryAsync<dynamic>("SELECT t.* FROM `travel-sample` t WHERE t.type=$1 LIMIT 5",
                 options =>
                 {
                     options.Parameter("landmark");
@@ -48,11 +117,11 @@ namespace ExamCouchbase
                 Console.WriteLine($"Complete! resultCount: {resultCount}");
 
                 // check exist primary index
-                var indexResult = await cluster.QueryAsync<dynamic>("SELECT name FROM system:indexes WHERE name ='def_primary'and is_primary=true and keyspace_id='travel-sample'").ConfigureAwait(false);
-                
+                var indexResult = await iCluster.QueryAsync<dynamic>("SELECT name FROM system:indexes WHERE name ='def_primary'and is_primary=true and keyspace_id='travel-sample'").ConfigureAwait(false);
+
                 await foreach (var row in indexResult)
                 {
-                    Console.WriteLine(row);                    
+                    Console.WriteLine(row);
                 }
             }
             catch (AuthenticationFailureException ex)
